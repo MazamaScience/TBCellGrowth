f299772b1b607727cb64935d968d44cbd291dd42
Will Leahy <willl@shuksan.domain>
2015-05-21 11:51:33 -0700

    Replaced "frame" with "image"

--- R/labelColonies.R
+++ R/labelColonies.R
@@ -1,16 +1,16 @@
 #' Searches an image for dark cell colonies and incrementally labels each colony.
 #' @export
-#' @param frame an image matrix to search for cell colonies
+#' @param image an image matrix to search for cell colonies
 #' @param artifactMask a mask of non biological features to ignore. \link{createArtifactMask}
 #' @return A \code{matrix} of integer labeled blobs.
 
-labelColonies <- function(frame, artifactMask) {
+labelColonies <- function(image, artifactMask) {
   
-  print("Searching new frame...")
+  print("Searching new image...")
   ptm <- proc.time()
   
   # Calculate homogeneity
-  homog <- glcm::glcm(frame, n_grey=25, window=c(3,3), statistics=c("homogeneity"))[,,1]
+  homog <- glcm::glcm(image, n_grey=25, window=c(3,3), statistics=c("homogeneity"))[,,1]
   
   # This essentially finds edges
   homog <- (1-homog)^3
@@ -23,53 +23,53 @@ labelColonies <- function(frame, artifactMask) {
   
   # Multiplying the edges by the original image darkens only the edges of the bacteria.
   # This reduces the chance of dark regions in the background being captured.
-  frameEdit <- (homog * frame)
+  imageEdit <- (homog * image)
   
   # Apply threshold
-  frameEdit <- frameEdit < 0.25
+  imageEdit <- imageEdit < 0.25
   
   # Reapply artifact mask
-  frameEdit[artifactMask>0] <- 0
+  imageEdit[artifactMask>0] <- 0
   
   # Mild dilate/erode to close small gaps
-  frameEdit <- closingGreyScale(frameEdit, makeBrush(7, shape='disc'))
+  imageEdit <- closingGreyScale(imageEdit, makeBrush(7, shape='disc'))
   
   # Remove very small blobs
-  frameEdit <- removeBlobs(frameEdit, 10)
+  imageEdit <- removeBlobs(imageEdit, 10)
   
   # Larger dilate/erode. This lets groups capture small bits that should
   # count as the same group.
-  frameEdit <- closingGreyScale(frameEdit, makeBrush(11, shape='disc'))
+  imageEdit <- closingGreyScale(imageEdit, makeBrush(11, shape='disc'))
   
   # Now remove medium blobs.
-  frameEdit <- removeBlobs(frameEdit, 25)
+  imageEdit <- removeBlobs(imageEdit, 25)
   
   # Large dilate to garuntee all of the area around the bacteria is selected.
-  frameEdit <- dilateGreyScale(frameEdit, makeBrush(15, shape='disc'))
+  imageEdit <- dilateGreyScale(imageEdit, makeBrush(15, shape='disc'))
   
   # Equalize m, which flattens the value histogram. This greatly increases the
   # contrast around the edges. Then select the lighter areas from that region and
-  frameEdit[(equalize(frame)) > 0.5] <- 0
+  imageEdit[(equalize(image)) > 0.5] <- 0
   
   # Remove noise
-  frameEdit <- removeBlobs(frameEdit, 20)
+  imageEdit <- removeBlobs(imageEdit, 20)
   
   # Dilate to bring groups together
-  frameEdit <- dilateGreyScale(frameEdit, makeBrush(3, shape='disc'))
+  imageEdit <- dilateGreyScale(imageEdit, makeBrush(3, shape='disc'))
   
   # Now label sections. It's important to label while still dilated so that labels
   # Are less sensitive.
-  frameEdit <- bwlabel(frameEdit)
+  imageEdit <- bwlabel(imageEdit)
   
   # Since sections are already labeled it's safe to remove a lot of the excess
   # White so we get a more accurate reading
-  frameEdit[(equalize(frame)^0.5) > 0.5] <- 0
+  imageEdit[(equalize(image)^0.5) > 0.5] <- 0
   
-  frameEdit[artifactMask>0] <- 0
+  imageEdit[artifactMask>0] <- 0
   
   print(proc.time() - ptm)
   
-  return(frameEdit)
+  return(imageEdit)
   
 }
 
