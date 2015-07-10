1c99dc71d2237577a33f87c4e172bb990511c83d
Will Leahy <willl@shuksan.domain>
2015-07-10 15:48:36 -0700

    flow_alignImages cleanup of old comments

--- R/flow_alignImages.R
+++ R/flow_alignImages.R
@@ -16,30 +16,12 @@
 
 flow_alignImages <- function(phase, dyes, alignmentTargets, targetWidth=25, 
                                searchSpace=25) {
-  
-  dyeNames <- names(dyes)
-  
-#   # Rotate images
-#   phase <- lapply(phase, EBImage::rotate, rotation)
-#   dyes <- lapply(dyes, function(dye) lapply(dye, EBImage::rotate, rotation))
-#   
-  # Cropping helper function
-  my_crop <- function(x, offset.x, offset.y, width, height) { 
-    return(x[(cropBoundaries[[2]] + offset.x):(width + offset.x - cropBoundaries[[4]]),
-             (cropBoundaries[[3]] + offset.y):(height + offset.y - cropBoundaries[[1]])])
-  }
-  
-  # Iterates through dyes and applies cropping to each set
-  my_dyeCrop <- function(dye, i, ...) {
-    dye[[i]] <- my_crop(dye[[i]], ...)
-    return(dye)
-  }
+ 
   
   # Sample the background image with alignment targets
   bgSamples <- lapply(alignmentTargets, function(x) phase[[1]][(x[[1]]-targetWidth):(x[[1]]+targetWidth),
                                                   (x[[2]]-targetWidth):(x[[2]]+targetWidth)])
   
-
   # Vectors detailing how much to shift images
   offset.x <- numeric(length(phase))
   offset.y <- numeric(length(phase))  
@@ -102,22 +84,6 @@ flow_alignImages <- function(phase, dyes, alignmentTargets, targetWidth=25,
     }
     
   }
-    
-# 
-#     # Crop phase
-#     phase[[i]] <- my_crop(image, offset.x, offset.y, dim(image)[[1]], dim(image)[[2]])
-#     # Crop Dyes
-#     lapply(dyes, my_dyeCrop, i, offset.x, offset.y, dim(image)[[1]], dim(image)[[2]])
-#     
-  
-#   
-#   # Crop background image
-#   phase[[1]] <- my_crop(phase[[1]], 0, 0, dim(phase[[1]])[[1]], dim(phase[[1]])[[2]])
-#   
-#   # Crop dye backgrounds
-#   dyes <- lapply(dyes, my_dyeCrop, 1, 0, 0, dim(phase[[1]])[[1]], dim(phase[[1]])[[2]])
-#   names(dyes) <- dyeNames
-#   
   
   return(list(phase=phase, dyes=dyes))
   
