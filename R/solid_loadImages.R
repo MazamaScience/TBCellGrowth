efd7c22b1bd372fb90c08a036c7b4b2514c46e96
Will Leahy <willl@shuksan.domain>
2015-07-14 12:32:05 -0700

    Added solid_loadImages, a different protocol for loading files based on the directory format for the new solid dataset. solid_ and flow_loadImages might be merged in the future.

---
+++ R/solid_loadImages.R
@@ -0,0 +1,44 @@
+#' @export
+#' @title Load Image Data into a List
+#' @param dataDir path to the directory containing images
+#' @param xy the xy number to load e.g. "xy2"
+#' @param channels which channels to load for the given xy
+#' @param channelNames a vector of names corresponding to the
+#' given channels
+#' @param ext file extension
+#' @param start index at which to start loading files
+#' @param n number of images to load into memory (defaults to all)
+#' @description This function uses the \pkg{EBImage::readimage} function
+#' to read in a series of images. Filenames for images are assumed to be
+#' ordered (e.g. with numeric indices). The return list contains
+#' two attributes:
+
+solid_loadImages <- function(dataDir, xy, channels, channelNames, ext="tif", start=1, n=NA) {
+  
+  readf <- function(im) {
+    return(EBImage::readImage(im)@.Data)
+  }
+  
+  # List all time folders
+  times <- list.files(dataDir)
+  
+  # Jump to start
+  times <- times[start:length(times)]
+  
+  # Subset times if necessary
+  if (!is.na(n)) {
+    times <- times[1:n]
+  }
+  
+  # Initialize images
+  images <- list()
+  
+  for (channel in channels) {
+    images[[channel]] <- lapply(times, function(t) readf(paste0(dataDir,"/",t,"/",xy,channel,".",ext)))
+  }
+  
+  names(images) <- channelNames
+  
+  return(images)
+  
+}
